6a65c4eefbcb5d827684ca9fad6ccba85f52d34e
AllenXie <771845392@qq.com>
2022-03-27 23:19:39 +0800

    表单组件

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>I'm User page</div>
+    <div class="manage">
+        <el-dialog
+            :title="operateType === 'add' ? 'add User' : 'update User'"
+            :visible.sync="isShow"
+        >
+            <Form :formLabel="">
+
+            </Form>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
+import Form from "../components/Form"
 export default {
     name: 'User',
+    components:{
+        Form  
+    },
     data() {
         return {
-            
+            operateType: 'add',
+            isShown: false
         }
     },
 }
